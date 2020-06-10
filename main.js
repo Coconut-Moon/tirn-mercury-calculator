54a78c840061ac5eca26af6364ee7afe50489916
Peter Russo <parusso@gmail.com>
2020-06-10 11:30:06 -0700

    Added serving selector

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Store user's weight and units
-
 function storeWeight() {
   var weight = document.getElementById('weight').value;
 
@@ -20,23 +19,46 @@ function storeWeight() {
   }
 }
 
-// Fish filter
-function myFunction() {
+// Render list of fish from json
+$(function () {
+  var data = {};
+  $.getJSON("fish-list.json")
+    .done(function (dat) {
+      data = dat;
+      console.log("success", dat)
+      render();
+    })
+    .fail(function () {
+      console.log("error", arguments);
+    })
+    .always(function () {
+      console.log("complete");
+    });
+
+  function render() {
+    var template = $("#template").html();
+    var html = Mustache.render(template, data);
+    $('#fishList').html(html);
+  }
+});
+
+// Filter list of fish
+function filterFish() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
+  var input, filter, ul, li, checkbox, i, txtValue;
+  input = document.getElementById('filterFish');
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
+  ul = document.getElementById("fishList");
   li = ul.getElementsByTagName('li');
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
 
-    //a = li[i].getElementsByTagName("a")[0];
-    checkbox = li[i].getElementsByTagName("label")[0];
+    // a = li[i].getElementsByTagName("a")[0];
+    checkbox = li[i].getElementsByTagName('label')[0];
 
-    //txtValue = a.textContent || a.innerText;
-    txtValue = checkbox.textContent || checkbox.innerText;
+    // txtValue = a.textContent || a.innerText;
+    txtValue = checkbox.innerText;
 
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
@@ -47,9 +69,33 @@ function myFunction() {
 }
 
 
-$( document ).ready(function() {
-  console.log( "ready!" );
+//Render element if checkbox is checked
 
- 
+
+// The box's status has changed.
+$(document).on('change', '.fish', function () {
+  //Get checked values
+  event.preventDefault();
+  var searchIDs = $("#selectFish input:checkbox:checked").map(function(){
+    return $(this).val();
+  }).get(); // <----
+
+  var chosenFish = {
+      selectedFish: searchIDs
+  };
+
+  function render() {
+    var template = $("#selectedFishTemplate").html();
+    var html = Mustache.render(template, chosenFish);
+    $('#selectedFish').html(html);
+  }
+
+  render();
+
 
 });
+
+
+
+
+
