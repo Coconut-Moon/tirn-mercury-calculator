afcc6b6d471b179dd3fe9abc0bc68e10a0408d32
Peter Russo <parusso@gmail.com>
2020-08-16 10:33:05 -0700

    Remove is working!

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// ID of the Google Spreadsheet
+var spreadsheetID = '1Boy1UKFhf_wMjJ6wHHFWAEdRTZ3QxK0f9rntOSkUjmM';
+
+// Json URL
+var url =
+  'https://spreadsheets.google.com/feeds/list/' +
+  spreadsheetID +
+  '/1/public/full?alt=json';
+
+var googleFishData = []
+
+var selectedFish = []
+
 // Navigate
 $(function () {
   $(".navigate-fish-list").click(function () {
@@ -17,43 +30,12 @@ $(function () {
 });
 
 $(function () {
-    $("#showResults").click(function () {
-    $(".question-two").addClass("display-none")
-    $(".results").removeClass("display-none")
-    window.scrollTo(0, 0);
-  });
- });
-
-$(function () {
   $("#backToStart").click(function () {
     $(".results").addClass("display-none")
     $(".question-one").removeClass("display-none")
     window.scrollTo(0, 0);
   });
 });
-
-
-
-// Store user's weight and units for later calculation
-function storeWeight() {
-  const weight = document.getElementById('weight').value;
-
-  //TODO: comment out log troubleshooting
-  console.log(weight);
-
-  const weightUnits = document.getElementsByName('weightUnits');
-
-  //TODO: comment out log troubleshooting
-  for (let i = 0, length = weightUnits.length; i < length; i++) {
-    if (weightUnits[i].checked) {
-
-      console.log(weightUnits[i].value);
-
-      // only one radio can be logically checked, don't check the rest
-      break;
-    }
-  }
-}
 
 // Display the list of fish on the page
 
@@ -63,21 +45,51 @@ function renderFishList(googleFishData) {
   $('#fishList').html(html);
 }
 
-/* Get list of fish from google spreadsheet (owned by TIRN) */
+// Create the array of selected fish
+function createSelectedFishArray(googleFishData) {
 
-// ID of the Google Spreadsheet
-var spreadsheetID = '1Boy1UKFhf_wMjJ6wHHFWAEdRTZ3QxK0f9rntOSkUjmM';
+  // Check the fish data
+  googleFishData = googleFishData || [];
+  //console.log('googleFishData', googleFishData);
 
-// Json URL
-var url =
-  'https://spreadsheets.google.com/feeds/list/' +
-  spreadsheetID +
-  '/1/public/full?alt=json';
+  // Get Selected fish IDs
+  const selectedFishIDs = $("#fishList input:checkbox:checked").map(function () {
+    return $(this).data("id");
+  }).get();
+  console.log("selectedFishIDs", selectedFishIDs)
+
+  // Match selected fish to full dataset by id.
+  // Build new array of fish results to use to run the calculations
+  function returnSelectedFishData(selectedFishIDs) {
+
+    selectedFishIDs = selectedFishIDs || null;
+
+    const selectedFishArray = [];
+    // maps through the user's selections
+    selectedFishIDs.map(selectedFish => {
+      // maps through all of the the data
+      googleFishData.map(fish => {
+        // conditional to compare the fish
+        if (fish.id == selectedFish) {
+          // add the data to the results array
+          selectedFishArray.push(fish)
+          console.log("fish.id", fish.id)
+        }
+      })
+    })
+    return selectedFishArray;
+  }
+
+  selectedFish = returnSelectedFishData(selectedFishIDs);
+  console.log("selectedFish", selectedFish)
+
+}
+
+
+/* Create list of fish from google spreadsheet (owned by TIRN) */
 
 // Retrieve fish data
 $.getJSON(url, function (data) {
-
-  var googleFishData = []
 
   var entry = data.feed.entry;
 
@@ -85,101 +97,54 @@ $.getJSON(url, function (data) {
   $(entry).each(function () {
     googleFishData.push(
       {
-        "index": Number(this.gsx$id.$t),
+        "id": Number(this.gsx$id.$t),
         "name": this.gsx$fish.$t,
         "mercury": Number(this.gsx$mercurylevel.$t),
         "mercury-rating": this.gsx$mercuryrating.$t,
         "message": this.gsx$message.$t,
-        "index": Number(this.gsx$id.$t),
         "bycatch-level": this.gsx$bycatchlevel.$t,
         "bycatch-message": this.gsx$bycatchmessage.$t,
         "microplastics-message": this.gsx$microplasticsmessage.$t
       }
     )
   })
-  console.log("New fish data from the GOOOG", googleFishData)
+  console.log("googleFishData", googleFishData)
 
   // Map spreadsheet fish data to a json object
-  renderSelectedFishList(googleFishData);
+  createSelectedFishArray(googleFishData);
 
   // Populate the list of fish
   renderFishList(googleFishData);
 
 });
 
-// Display the list of selected fish
-function renderSelectedFishList(allFishData) {
+// Render serving selector for each selected fish when the checkbox status changes
+$(document).on('change', '.fish', function () {
 
-  // Container for fish data
-  allFishData = allFishData || [];
+  //Update selected Fish Array
+  createSelectedFishArray(googleFishData)
 
-  // Match selected fish to full dataset by INDEX.
-  // Build new array of fish results to use to run the calculations
-  function returnSelectedFishData(selectedFishIDs) {
-    selectedFishIDs = selectedFishIDs || null;
-    const selectedFishArray = [];
-    // maps through the user's selections
-    selectedFishIDs.map(selectedFish => {
-      // maps through all of the the data
-      allFishData.map(fish => {
-        // conditional to compare the fish
-        if (fish.index == selectedFish) {
-          // add the data to the results array
-          selectedFishArray.push(fish)
-          console.log("!!!!!! fish mapping results", fish.index)
-        }
-      })
-    })
-    return selectedFishArray;
+  // Send fish data to the template
+  function renderSelectedFishTemplate() {
+    var template = $("#selectedFishTemplate").html();
+    var html = Mustache.to_html(template, selectedFish);
+    $('#selectedFish').html(html);
   }
 
-  // Render serving selector for each selected fish when the checkbox status changes
-  $(document).on('change', '.fish', function () {
+  // Render the list of selected fish on the page
+  renderSelectedFishTemplate();
 
-    // This is the array of selected fish you are creating an array with to send to the returnSelectedFish function (logged out below)
-    const selectedFishIDs = $("#fishList input:checkbox:checked").map(function () {
-      return $(this).data("index");
-    }).get();
-    console.log("selectedFishIDs", selectedFishIDs)
+  // Enable/disable the results button
+  if ($(this).is(':checked')) {
+    //Enable the submit button.
+    $('#showResults').attr("disabled", false);
+  } else {
+    //If it is not checked, disable the button.
+    $('#showResults').attr("disabled", true);
+  }
 
-    selectedFish = returnSelectedFishData(selectedFishIDs);
+}) // End document change fish function
 
-    // THESE ARE YOUR FISHES!!!!!
-    console.log("These are your returned fishes", selectedFish)
-
-    // Send fish data to the template
-    function renderSelectedFishTemplate() {
-      var template = $("#selectedFishTemplate").html();
-      var html = Mustache.to_html(template, returnSelectedFishData(selectedFishIDs));
-      $('#selectedFish').html(html);
-    }
-
-    // Render the list of selected fish on the page
-    renderSelectedFishTemplate();
-
-    // Enable/disable the results button
-    if ($(this).is(':checked')) {
-      //Enable the submit button.
-      $('#showResults').attr("disabled", false);
-    } else {
-      //If it is not checked, disable the button.
-      $('#showResults').attr("disabled", true);
-    }
-
-  }); // End document change fish function
-
-  // calculate results when user clicks "Show Results"
-  document.getElementById("showResults").addEventListener("click", function (event) {
-
-    // Don't reload the page
-    event.preventDefault()
-
-    // Calculate results
-    calculateResults(selectedFish)
-
-  });
-
-} // End run fishapp
 
 /* Calculate Results of fish selection */
 
@@ -196,6 +161,8 @@ function calculateResults(selectedFish) {
 
   // In the selected fish list, pull the serving sizes and quantities into individual fish
   $('#selectedFish li').each(function () {
+
+    console.log($('#selectedFish li'))
 
     // Set selected serving amount
     const servingAmountWeekly = $("input[name='serving-amount']", this).map(function () {
@@ -225,7 +192,7 @@ function calculateResults(selectedFish) {
     });
   });
 
-  console.log("Amounts array", fishAmounts);
+  //console.log("Amounts array", fishAmounts);
 
   // Merge our sizes and quantities with the selected fish
   const fishResults = fishAmounts.map((item, i) => Object.assign({}, item, selectedFish[i]));
@@ -243,15 +210,15 @@ function calculateResults(selectedFish) {
   let weightKilos = weight;
   if (weightUnits === "lbs") {
     weightKilos = weight * 0.4535924;
-  };
-  console.log("Weight in kilos", weightKilos);
+  }
+  //console.log("Weight in kilos", weightKilos);
 
   // Add daily mercury exposure per fish to Fish Results
   fishResults.forEach(function (element) {
     // For each fish, daily mercury exposure = (Mercury content * daily grams) / weight in kilos
     element.mercuryexposuredaily = (Number(element["mercury"]) * Number(element["serving-grams-daily"])) / weightKilos;
   });
-  console.log("fishResults, now with more mercury", fishResults);
+  //console.log("fishResults, now with more mercury", fishResults);
 
   //Set EPA recommended total daily mercury exposure (in grams per ounce of fish)
   const EPADailyExposureLimit = 0.1
@@ -267,18 +234,18 @@ function calculateResults(selectedFish) {
 
   // Calculate user's total daily mercury exposure
   const userDailyExposure = fishResults.sum("mercuryexposuredaily");
-  console.log("Mercury exposure sum total", userDailyExposure);
+  //console.log("Mercury exposure sum total", userDailyExposure);
 
   // Add percent of total mercury exposure to each fish in fishlist
   fishResults.forEach(function (element) {
     // For each fish, percent of mercury exposure = element.mercuryexposuredaily /userDailyExposure
     element.mercuryexposurepercent = Math.round((Number(element["mercuryexposuredaily"]) / userDailyExposure) * 100);
   });
-  console.log("fishResults, now with exposure percent", fishResults);
+  //console.log("fishResults, now with exposure percent", fishResults);
 
   // Calculate total mercury exposure compared to EPA limit
   const userTotalExposurePercent = (userDailyExposure / EPADailyExposureLimit) * 100;
-  console.log("Percentage consumed daily", userTotalExposurePercent);
+  //console.log("Percentage consumed daily", userTotalExposurePercent);
 
   // ------------- Render some values on the  results page and footer - separate function?
 
@@ -296,54 +263,53 @@ function calculateResults(selectedFish) {
   // Le new chart
 
   var totalMercuryOverage = userDailyExposure - EPADailyExposureLimit;
-  console.log('total mercury overage',totalMercuryOverage )
+  //console.log('total mercury overage',totalMercuryOverage )
 
   // Calculate height of the overage bar
   var chartExposureOverageBar = Math.max(0, Math.floor(((userDailyExposure - EPADailyExposureLimit) / userDailyExposure) * 100));
-  console.log("overage height",chartExposureOverageBar+ '%');
+  //console.log("overage height",chartExposureOverageBar+ '%');
 
   // Calculate height of the recommended bar bar
   var chartExposureRecBar = Math.max(0, Math.floor((userDailyExposure / EPADailyExposureLimit) * 100));
-  console.log("rec height",chartExposureRecBar + '%');
+  //console.log("rec height",chartExposureRecBar + '%');
 
   // Use jquery to set the values appropriately
   var chartOverage = $('#mercuryExposureOverageBar');
   var chartRec = $('#mercuryExposureRecBar');
 
-  if (userDailyExposure > EPADailyExposureLimit)
-    {
+  if (userDailyExposure > EPADailyExposureLimit) {
 
-      // Display the "Total exposure" message
-      document.getElementById("mercuryExposureMessage").innerHTML = `${Math.round(totalMercuryOverage * 1000)}% over`;
-      console.log('total mercury overage',totalMercuryOverage )
+    // Display the "Total exposure" message
+    document.getElementById("mercuryExposureMessage").innerHTML = `${Math.round(totalMercuryOverage * 1000)}% over`;
+    //console.log('total mercury overage',totalMercuryOverage )
 
-      $(chartOverage)
-        .attr('height', chartExposureOverageBar + '%')
-        .attr('y', 0);
-      $(chartRec)
-        .attr('height', chartExposureRecBar + '%')
-        .attr('y', chartExposureOverageBar + '%');
+    $(chartOverage)
+      .attr('height', chartExposureOverageBar + '%')
+      .attr('y', 0);
+    $(chartRec)
+      .attr('height', chartExposureRecBar + '%')
+      .attr('y', chartExposureOverageBar + '%');
 
-      // Change the first text element
-      $('#chartPercentage100').text(Math.floor(userTotalExposurePercent) + '%');
-      $('#chartPercentage80').text(Math.floor(userTotalExposurePercent * .8) + '%');
-      $('#chartPercentage60').text(Math.floor(userTotalExposurePercent * .6) + '%');
-      $('#chartPercentage40').text(Math.floor(userTotalExposurePercent * .4) + '%');
-      $('#chartPercentage20').text(Math.floor(userTotalExposurePercent * .2) + '%');
+    // Change the first text element
+    $('#chartPercentage100').text(Math.floor(userTotalExposurePercent) + '%');
+    $('#chartPercentage80').text(Math.floor(userTotalExposurePercent * .8) + '%');
+    $('#chartPercentage60').text(Math.floor(userTotalExposurePercent * .6) + '%');
+    $('#chartPercentage40').text(Math.floor(userTotalExposurePercent * .4) + '%');
+    $('#chartPercentage20').text(Math.floor(userTotalExposurePercent * .2) + '%');
 
-    } else {
+  } else {
 
-      // Display the "Total exposure" message
-      document.getElementById("mercuryExposureMessage").innerHTML = `${Math.round(userTotalExposurePercent)}% of`;
+    // Display the "Total exposure" message
+    document.getElementById("mercuryExposureMessage").innerHTML = `${Math.round(userTotalExposurePercent)}% of`;
 
-      $(chartOverage)
-        .attr('height', 0)
-        .attr('y', 0);
-      $(chartRec)
-        .attr('height', chartExposureRecBar + '%')
-        .attr('y', Math.max(0, (100 - chartExposureRecBar)) + '%');
+    $(chartOverage)
+      .attr('height', 0)
+      .attr('y', 0);
+    $(chartRec)
+      .attr('height', chartExposureRecBar + '%')
+      .attr('y', Math.max(0, (100 - chartExposureRecBar)) + '%');
 
-    }
+  }
 
   // --- render the results cards - separate function?
 
@@ -352,7 +318,7 @@ function calculateResults(selectedFish) {
     var template = $("#fishResultsTemplate").html();
     var html = Mustache.render(template, fishResults);
     $('#fishResults').html(html);
-    console.log("Fish Results, as rendered", fishResults);
+    //console.log("Fish Results, as rendered", fishResults);
   }
   renderResults();
 
@@ -364,48 +330,44 @@ function calculateResults(selectedFish) {
   }
   renderRawData();
 
-  // Update the user inputs on the results page to match those on the selection page
 
-  // Select lists - generic function to change selected value
-  function selectElement(id, valueToSelect) {
-    let element = document.getElementById(id);
-    element.value = valueToSelect;
-  }
-
-  // Set fish results select lists to correct amounts via a data attribute
-  $('#fishResults select').each(function () {
-    selectElement($(this).attr('id'), $(this).data('servings'))
-  });
-
-}; // End calculate results
+} // End calculate results
 
 // TODO: Reset fish results and recalculate everything when we:
 // Click "calculate results", remove a fish from results, change the servings, change the units, we want to:
 // Recalculate results
 // Update fish results
 // Update fish selections
-// Remove fish
 
 // Remove fish when x is clicked
-function removeFish(item) {
-  var fishToRemove = $(item).data('remove')
-  console.log("fish we wanna remove", fishToRemove)
+function removeFish(fishToRemove) {
+  console.log("fishToRemove", fishToRemove)
   $("#selectedFish_" + fishToRemove).remove()
   $("#fish_" + fishToRemove).prop("checked", false)
   $("#fishResult_" + fishToRemove).remove()
 
-    renderSelectedFishList();
-    calculateResults(selectedFish);
-};
+  console.log("original selectedFish", selectedFish);
+  selectedFish.map((thisFish, i) => {
+    if (thisFish.id === fishToRemove) {
+      console.log("thisFish.id", thisFish.id)
+      console.log("i", i)
+      selectedFish.splice(i, 1)
+    }
+
+  })
+
+  //createSelectedFishArray();
+  console.log("updated selectedFish", selectedFish);
+  console.log("selectedFish[fishToRemove]", selectedFish[fishToRemove]);
+  calculateResults(selectedFish);
+}
 
 // Recalculate results when any input changes (may need to target this better)
-$(document).change(function () {
-  $('.input').change(function(){
-    //renderSelectedFishList(data);
-    calculateResults(selectedFish);
-    console.log('Inputs bound');
-  });
-
+$(document).on('change', '.input', function () {
+  //createSelectedFishArray(data);
+  calculateResults(selectedFish);
+  console.log('Inputs bound');
+  console.log('selectedFish input', selectedFish);
 });
 
 // Filter list of fish
@@ -492,12 +454,40 @@ function sortTable(n) {
 
 $(document).ready(function () {
 
+  // calculate results when user clicks "Show Results"
+  document.getElementById("showResults").addEventListener("click", function (event) {
+
+    // Don't reload the page
+    event.preventDefault()
+
+    // Calculate results
+    calculateResults(selectedFish)
+
+    // Update the user inputs on the results page to match those on the selection page
+
+    // Select lists - generic function to change selected value
+    function selectElement(id, valueToSelect) {
+      let element = document.getElementById(id);
+      element.value = valueToSelect;
+    }
+
+    // Set fish results select lists to correct amounts via a data attribute
+    $('#fishResults select').each(function () {
+      selectElement($(this).attr('id'), $(this).data('servings'))
+    });
+
+    // Hide selection page/show results
+    $(".question-two").addClass("display-none")
+    $(".results").removeClass("display-none")
+    window.scrollTo(0, 0);
+  })
+
   // Accordion
   var acc = document.getElementsByClassName("accordion");
   var i;
 
   for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener("click", function () {
       this.classList.toggle("active");
       var panel = this.nextElementSibling;
       if (panel.style.display === "block") {
@@ -511,7 +501,7 @@ $(document).ready(function () {
   /* Modal controls */
   // Get the modal
   var modal = document.getElementById("servingModal");
-  console.log("get modal", document.getElementById("servingModal"))
+  //console.log("get modal", document.getElementById("servingModal"))
   // Get the button that opens the modal
   var btn = document.getElementById("openServingModal");
   // Get the <span> element that closes the modal
@@ -535,4 +525,3 @@ $(document).ready(function () {
   }
 
 });
-
